ebb1e6f899c03ec43f6fcfb1384453a5aaa9a24a
chosmin4 <chosmin4@naver.com>
2021-10-25 20:54:10 +0900

    1 시작

---
+++ lv10_project_LMS/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 학생 관리 시스템 - 다만 상속을 쓰는
+		 * 
+		 * 1. 학생 - 이름, 코드
+		 * 2. 과목 - 과목 이름, (코드), 점수
+		 * 
+		 * 있어야하는 기능
+		 * 학생 등록 (new Student)
+		 * 수강 신청 (new Subject)
+		 * 성적 관리 (score 추가 또는 수정)
+		 * 
+		 */
+
+	}
+
+}

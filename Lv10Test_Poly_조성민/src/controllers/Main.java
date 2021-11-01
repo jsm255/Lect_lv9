f5f8d7f7af152c920396cb8458863f1255618fd3
chosmin4 <chosmin4@naver.com>
2021-11-01 18:07:28 +0900

    1 스타트

---
+++ Lv10Test_Poly_조성민/src/controllers/Main.java
@@ -0,0 +1,26 @@
+package controllers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Lv10 Test
+		
+		// 다형성rpg
+		// A to Z 까지
+		// 시간기록
+		
+		// 시작 16:06
+		// 종료 :
+		// 소요 :
+		
+		// 프로젝트명 : Lv10Test_Poly_조성민
+		// 패키지 : models, controller(Main포함)
+		// 멤버 : 모두 캡슐화(private)
+		
+		// 빈 프로젝트, 패키지 생성 후 커밋
+		// 보고후 진행
+
+	}
+
+}

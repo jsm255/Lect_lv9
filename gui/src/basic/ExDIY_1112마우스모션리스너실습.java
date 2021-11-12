c5b7f8fd0b56b1b04746388b23fae2bcfb400295
chosmin4 <chosmin4@naver.com>
2021-11-12 16:33:58 +0900

    211112 마우스모션리스너실습

--- gui/src/basic/ExDIY_1112마우스모션리스너실습.java
+++ gui/src/basic/ExDIY_1112마우스모션리스너실습.java
@@ -1,7 +1,9 @@
 package basic;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
@@ -52,16 +54,97 @@ class MMLNemo {
 	}
 }
 
-class MMLPanel extends JPanel implements MouseMotionListener{
+class MMLPanel extends JPanel implements MouseListener, MouseMotionListener{
+	
+	private MMLNemo nemo;
+	
+	private boolean moving;
+	
+	private int preX;
+	private int preY;
+	
+	public MMLPanel() {
+	
+		setLayout(null);
+		setBounds(0, 0, 700, 700);
+		
+		this.nemo = new MMLNemo(300,300,100,100);
+		
+		addMouseMotionListener(this);
+		addMouseListener(this);
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(this.nemo.getC());
+		g.drawRect(this.nemo.getX(), this.nemo.getY(), this.nemo.getW(), this.nemo.getH());
+		
+		repaint();
+	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
+		if(this.moving) {
+			int x = e.getX();
+			int y = e.getY();
+			if(this.preX != x || this.preY != y) {
+				this.nemo.setX(this.nemo.getX() + (x - this.preX));
+				this.nemo.setY(this.nemo.getY() + (y - this.preY));
+				this.preX = x;
+				this.preY = y;
+			}
+		}
 		
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		
+		if(x >= this.nemo.getX() && x < this.nemo.getX() + this.nemo.getW() &&
+				y >= this.nemo.getY() && y < this.nemo.getY() + this.nemo.getH()) {
+			this.moving = true;
+			this.preX = x;
+			this.preY = y;
+		}
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		this.moving = false;
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}

383fd757ee9ab7d25721982088373206d64b8a06
chosmin4 <chosmin4@naver.com>
2021-11-17 18:25:08 +0900

    211117 소코반 완성

--- gui_sokoban/src/controller/GamePanel.java
+++ gui_sokoban/src/controller/GamePanel.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -29,6 +30,9 @@ public class GamePanel extends Utility{
 	private int pY = 2;
 	private int pX = 2;
 	
+	private boolean gameClear = false;
+	private boolean popUp = false;
+	
 	private ImageIcon boxImage = new ImageIcon(new ImageIcon("image/box.png").
 			getImage().getScaledInstance(BLOCK, BLOCK, Image.SCALE_SMOOTH));
 	private ImageIcon guyImage = new ImageIcon(new ImageIcon("image/guy.png").
@@ -46,6 +50,8 @@ public class GamePanel extends Utility{
 							{WALL,EMPTY,EMPTY,EMPTY,GOAL,EMPTY,EMPTY,WALL}};
 	
 	private int[][] mapTemp = new int[SIZE][SIZE];
+	
+	private int[][] goalMemo = {{2,1},{3,5},{4,1},{5,4},{6,3},{6,6},{7,4}};
 			
 	public GamePanel() {
 		setLayout(null);
@@ -60,8 +66,19 @@ public class GamePanel extends Utility{
 	}
 
 	private void makeButtonAndLabel() {
-		// TODO Auto-generated method stub
+		this.reset.setText("Restart(r)");
+		this.reset.setBounds(100, 50, 100, 75);
+		this.reset.addActionListener(this);
 		
+		add(this.reset);
+		
+		this.label.setBounds(300, 25, 200, 100);
+		this.label.setHorizontalAlignment(JLabel.CENTER);
+		this.label.setVerticalAlignment(JLabel.CENTER);
+		this.label.setFont(new Font("",Font.BOLD,30));
+		this.label.setText("SOKOBAN");
+		
+		add(this.label);
 	}
 
 	private void generateMap() {	// 리셋할때 갖다 복사
@@ -75,6 +92,12 @@ public class GamePanel extends Utility{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		checkMap();
+		if(this.gameClear && this.popUp) {
+			new FinishFrame();
+			this.popUp = false;
+		}
 		
 		int x = 50;
 		int y = 150;
@@ -109,26 +132,97 @@ public class GamePanel extends Utility{
 			x = 50;
 			y += 50;
 		}
-		
 		requestFocusInWindow();
 		repaint();
 	}
 	
+	private void checkMap() {
+		int arriveCnt = 0;
+		for(int i = 0; i<this.goalMemo.length; i++) {
+			int goalY = this.goalMemo[i][0];
+			int goalX = this.goalMemo[i][1];
+			
+			if(this.map[goalY][goalX] == EMPTY) this.map[goalY][goalX] = GOAL;
+			else if(this.map[goalY][goalX] == ARRIVED) arriveCnt ++;
+		}
+		
+		if(arriveCnt == 7) {
+			if(!this.gameClear && !this.popUp) {
+				this.gameClear = true;
+				this.popUp = true;
+			}
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		super.actionPerformed(e);
+		if(e.getSource() == this.reset) {
+			resetGame();
+		}
 	}
 	
+	private void resetGame() {
+		this.gameClear = false;
+		this.popUp = false;
+		
+		this.pY = 2;
+		this.pX = 2;
+		
+		for(int i = 0; i<this.SIZE; i++) {
+			for(int j = 0; j<this.SIZE; j++) {
+				this.map[i][j] = this.mapTemp[i][j];
+			}
+		}
+	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		super.keyPressed(e);
-	}
-	
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		super.keyReleased(e);
+		if(e.getKeyCode() == e.VK_R) resetGame();
+		else {
+			
+			int tempY = this.pY;
+			int tempX = this.pX;
+			
+			if(e.getKeyCode() == e.VK_UP) tempY --;
+			else if(e.getKeyCode() == e.VK_DOWN) tempY ++;
+			else if(e.getKeyCode() == e.VK_LEFT) tempX --;
+			else if(e.getKeyCode() == e.VK_RIGHT) tempX ++;
+			
+			// 맵 밖으로 나갔으면
+			if(tempY < 0 || tempY > this.SIZE-1 || tempX < 0 || tempX > this.SIZE-1) return;
+			else {
+				// 벽에 들이박으면
+				if(this.map[tempY][tempX] == WALL) return;
+				
+				// 앞에 박스가 있으면
+				else if(this.map[tempY][tempX] == BOX || this.map[tempY][tempX] == ARRIVED) {
+					
+					// 박스가 밀렸을 때의 위치를 봐야하므로 한 칸 밀어줌
+					int boxTempY = tempY != this.pY ? tempY+(tempY-this.pY) : tempY;
+					int boxTempX = tempX != this.pX ? tempX+(tempX-this.pX) : tempX;
+					
+					// 근데 그 박스가 벽에 들이박거나 그 앞에 또 박스가 있으면
+					if(this.map[boxTempY][boxTempX] == WALL ||
+							this.map[boxTempY][boxTempX] == BOX) return;
+					else {
+						
+						// 박스가 도착점에 갔으면
+						if(this.map[boxTempY][boxTempX] == GOAL)
+							this.map[boxTempY][boxTempX] = ARRIVED;
+						else {
+							
+							// 빈 공간이면
+							// ※ 박스는 어차피 밀어야 움직이므로 빈공간 처리해줄 필요는 없음
+							this.map[boxTempY][boxTempX] = BOX;
+						}
+					}
+				}
+				
+				this.map[this.pY][this.pX] = EMPTY;
+				this.map[tempY][tempX] = PLAYER;
+				this.pY = tempY;
+				this.pX = tempX;
+			}
+		}
 	}
 }

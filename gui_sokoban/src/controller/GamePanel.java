2359df7a2cd90d00b61171895960dca5440fe358
chosmin4 <chosmin4@naver.com>
2021-11-17 19:33:17 +0900

    211117 랜덤 소코반

--- gui_sokoban/src/controller/GamePanel.java
+++ gui_sokoban/src/controller/GamePanel.java
@@ -7,12 +7,15 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class GamePanel extends Utility{
+	
+	private Random ran = new Random();
 	
 	private JButton reset = new JButton();
 	private JLabel label = new JLabel();
@@ -27,6 +30,11 @@ public class GamePanel extends Utility{
 	private final int SIZE = 8;
 	private final int BLOCK = 50;
 	
+	private int stage = 1;
+	
+	private int goalCnt = 7;
+	private int startPY = 2;
+	private int startPX = 2;
 	private int pY = 2;
 	private int pX = 2;
 	
@@ -57,7 +65,7 @@ public class GamePanel extends Utility{
 		setLayout(null);
 		setBounds(0, 0, 800, 700);
 		
-		generateMap();
+		tempMap();
 		makeButtonAndLabel();
 		
 		setFocusable(true);
@@ -81,7 +89,7 @@ public class GamePanel extends Utility{
 		add(this.label);
 	}
 
-	private void generateMap() {	// 리셋할때 갖다 복사
+	private void tempMap() {	// 리셋할때 갖다 복사
 		for(int i = 0; i<SIZE; i++) {
 			for(int j = 0; j<SIZE; j++) {
 				this.mapTemp[i][j] = this.map[i][j];
@@ -93,11 +101,16 @@ public class GamePanel extends Utility{
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		checkMap();
+		if(this.stage == 1) checkMapForStage1();
+		else checkMap();
 		if(this.gameClear && this.popUp) {
 			new FinishFrame();
-			this.popUp = false;
+			this.stage ++;
+			randomStageGenerator();
 		}
+		
+		g.setFont(new Font("",Font.BOLD, 20));
+		g.drawString(String.valueOf("Stage "+this.stage), 600, 75);
 		
 		int x = 50;
 		int y = 150;
@@ -146,7 +159,106 @@ public class GamePanel extends Utility{
 			else if(this.map[goalY][goalX] == ARRIVED) arriveCnt ++;
 		}
 		
-		if(arriveCnt == 7) {
+		if(arriveCnt == this.goalCnt) {
+			if(!this.gameClear && !this.popUp) {
+				this.gameClear = true;
+				this.popUp = true;
+			}
+		}
+	}
+
+	private void randomStageGenerator() {
+		this.map = new int[SIZE][SIZE];
+		
+		int boxs = ran.nextInt(3)+4;
+		int goals = boxs;
+		int walls = ran.nextInt(2)+4;
+		for(int i = 0; i<walls; i++) {
+			int ranY = ran.nextInt(SIZE);
+			int ranX = ran.nextInt(SIZE);
+			
+			if(this.map[ranY][ranX] == EMPTY) {
+				int wallCnt = 0;
+				for(int j = -1; j<=1; j++) {
+					if(ranY + j < 0 || ranY + j > SIZE - 1) {}
+					else {
+						if(this.map[ranY+j][ranX] == WALL) wallCnt ++;
+					}
+					if(ranX + j < 0 || ranX + j > SIZE - 1) {}
+					else {
+						if(this.map[ranY][ranX+j] == WALL) wallCnt ++;
+					}
+				}
+				if(wallCnt >= 1) i--;
+				else this.map[ranY][ranX] = WALL;
+			}
+			else i --;
+		}
+		
+		for(int i = 0; i<boxs; i++) {
+			int ranY = ran.nextInt(SIZE-2) + 1;	// 움직일 수는 있어야하므로
+			int ranX = ran.nextInt(SIZE-2) + 1;
+			
+			if(this.map[ranY][ranX] == EMPTY) {
+				int wallCnt = 0;
+				for(int j = -1; j<=1; j++) {
+					if(this.map[ranY+j][ranX] == WALL || this.map[ranY+j][ranX] == BOX)
+						wallCnt ++;
+					if(this.map[ranY][ranX+j] == WALL || this.map[ranY+j][ranX] == BOX)
+						wallCnt ++;
+				}
+				if(wallCnt >= 2) i--;
+				else this.map[ranY][ranX] = BOX;
+			}
+			else i --;
+		}
+		
+		this.goalMemo = new int[goals][2];
+		this.goalCnt = goals;
+		for(int i = 0; i<goals; i++) {
+			int ranY = ran.nextInt(SIZE);
+			int ranX = ran.nextInt(SIZE);
+			
+			if(this.map[ranY][ranX] == EMPTY) this.map[ranY][ranX] = GOAL;
+			else if(this.map[ranY][ranX] == BOX) this.map[ranY][ranX] = ARRIVED;
+			else {
+				i --;
+				continue;
+			}
+			this.goalMemo[i][0] = ranY;
+			this.goalMemo[i][1] = ranX;
+		}
+		
+		while(true) {
+			int ranY = ran.nextInt(SIZE);
+			int ranX = ran.nextInt(SIZE);
+			
+			if(this.map[ranY][ranX] == EMPTY) {
+				this.map[ranY][ranX] = PLAYER;
+				this.pY = ranY;
+				this.pX = ranX;
+				this.startPY = ranY;
+				this.startPX = ranX;
+				break;
+			}
+		}
+		
+		this.gameClear = false;
+		this.popUp = false;
+		tempMap();
+	}
+	
+	private void checkMapForStage1() {
+		int arriveCnt = 0;
+		for(int i = 0; i<this.goalMemo.length; i++) {
+			int goalY = this.goalMemo[i][0];
+			int goalX = this.goalMemo[i][1];
+			
+			if(this.map[goalY][goalX] == EMPTY) this.map[goalY][goalX] = GOAL;
+			else if(this.map[goalY][goalX] == ARRIVED) arriveCnt ++;
+		}
+		
+		if(arriveCnt == this.goalCnt) {
 			if(!this.gameClear && !this.popUp) {
 				this.gameClear = true;
 				this.popUp = true;
@@ -165,8 +277,8 @@ public class GamePanel extends Utility{
 		this.gameClear = false;
 		this.popUp = false;
 		
-		this.pY = 2;
-		this.pX = 2;
+		this.pY = this.startPY;
+		this.pX = this.startPX;
 		
 		for(int i = 0; i<this.SIZE; i++) {
 			for(int j = 0; j<this.SIZE; j++) {
@@ -201,19 +313,25 @@ public class GamePanel extends Utility{
 					int boxTempY = tempY != this.pY ? tempY+(tempY-this.pY) : tempY;
 					int boxTempX = tempX != this.pX ? tempX+(tempX-this.pX) : tempX;
 					
-					// 근데 그 박스가 벽에 들이박거나 그 앞에 또 박스가 있으면
-					if(this.map[boxTempY][boxTempX] == WALL ||
-							this.map[boxTempY][boxTempX] == BOX) return;
+					// 그 박스가 필드 밖으로 나가면
+					if(boxTempY < 0 || boxTempY > this.SIZE-1 ||
+							boxTempX < 0 || boxTempX > this.SIZE-1) return;
 					else {
-						
-						// 박스가 도착점에 갔으면
-						if(this.map[boxTempY][boxTempX] == GOAL)
-							this.map[boxTempY][boxTempX] = ARRIVED;
+						// 근데 그 박스가 벽에 들이박거나 그 앞에 또 박스가 있으면
+						if(this.map[boxTempY][boxTempX] == WALL ||
+								this.map[boxTempY][boxTempX] == BOX ||
+								this.map[boxTempY][boxTempX] == ARRIVED) return;
 						else {
 							
-							// 빈 공간이면
-							// ※ 박스는 어차피 밀어야 움직이므로 빈공간 처리해줄 필요는 없음
-							this.map[boxTempY][boxTempX] = BOX;
+							// 박스가 도착점에 갔으면
+							if(this.map[boxTempY][boxTempX] == GOAL)
+								this.map[boxTempY][boxTempX] = ARRIVED;
+							else {
+								
+								// 빈 공간이면
+								// ※ 박스는 어차피 밀어야 움직이므로 빈공간 처리해줄 필요는 없음
+								this.map[boxTempY][boxTempX] = BOX;
+							}
 						}
 					}
 				}
